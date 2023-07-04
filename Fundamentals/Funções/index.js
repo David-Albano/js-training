5b99ae5014f91c1d89c929318e62b53c0dc6dda5
David Afonso Méndez <afonso27000@gmail.com>
2023-07-04 14:40:34 +0100

    Functions Accepting Callback Functions

--- Fundamentals/Funções/index.js
+++ Fundamentals/Funções/index.js
@@ -31,30 +31,60 @@
 
 // How Passing Arguments Works
 
-const flight = 'LH234'
-const david = {
-    name: 'David Afonso',
-    passport: 4293842349,
-}
+// const flight = 'LH234'
+// const david = {
+//     name: 'David Afonso',
+//     passport: 4293842349,
+// }
 
-const checkIn = function(flightNum, passenger) {
-    flightNum = 'LH999'
-    passenger.name = 'Mr. ' + passenger.name
+// const checkIn = function(flightNum, passenger) {
+//     flightNum = 'LH999'
+//     passenger.name = 'Mr. ' + passenger.name
 
-    if(passenger.passport === 4293842349) {
-        alert('Checked in')
-    } else {
-        alert('Wrong passport')
-    }
-}
+//     if(passenger.passport === 4293842349) {
+//         alert('Checked in')
+//     } else {
+//         alert('Wrong passport')
+//     }
+// }
 
+// // checkIn(flight, david)
+// // console.log(flight)
+// // console.log(david)
+
+// const newPassport = function(person) {
+//     person.passport = Math.trunc(Math.random() * 100000000)
+// }
+
+// newPassport(david)
 // checkIn(flight, david)
-// console.log(flight)
-// console.log(david)
 
-const newPassport = function(person) {
-    person.passport = Math.trunc(Math.random() * 100000000)
+
+const oneWord = function(str) {
+    return str.replace(/ /g, '').toLowerCase()
 }
 
-newPassport(david)
-checkIn(flight, david)
+const upperFirstWord = function(str) {
+    const [first, ...others] = str.split(' ')
+    return [first.toUpperCase(), ...others].join(' ')
+}
+
+// Higher-order function
+const transformer = function(str, fn) {
+    console.log(`Original string: ${str}`)
+    console.log(`Transformed string: ${fn(str)}`)
+
+    console.log(`Transformed by : ${fn.name}`)
+}
+
+transformer('JavaScript is the best!', upperFirstWord)
+transformer('JavaScript is the best!', oneWord)
+
+// JS uses callbacks all the time
+const high5 = function() {
+    console.log('👋')
+}
+
+document.body.addEventListener('click', high5);
+
+['John', 'Marta', 'Esteban', 'Laura'].forEach(high5)
