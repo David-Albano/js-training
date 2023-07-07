5c78369efa6c0c837435999fe5df846af206eb91
David Afonso Méndez <afonso27000@gmail.com>
2023-07-07 19:44:33 +0100

    Little practice with arrays

--- Fundamentals/Arrays/script.js
+++ Fundamentals/Arrays/script.js
@@ -53,45 +53,86 @@
 
 // forEach Loop
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
-// for(const movement of movements) {
-for(const [index, movement] of movements.entries()) {
-    if(movement > 0) {
-        console.log(`Movement ${index +1} You deposited ${movement}`);
-    } else {
-        console.log(`Movement ${index +1} You withdrew ${Math.abs(movement)}`);
-    }
+// // for(const movement of movements) {
+// for(const [index, movement] of movements.entries()) {
+//     if(movement > 0) {
+//         console.log(`Movement ${index +1} You deposited ${movement}`);
+//     } else {
+//         console.log(`Movement ${index +1} You withdrew ${Math.abs(movement)}`);
+//     }
+// }
+
+// console.log('~~~~ FOR EACH ~~~~')
+
+// // order for forEach loop --> current element, current index, entire array
+// // This is how the arguments are passed into the function
+// movements.forEach(function(movement, index, array) {
+//     if(movement > 0) {
+//         console.log(`Movement ${index +1} You deposited ${movement}`);
+//     } else {
+//         console.log(`Movement ${index +1} You withdrew ${Math.abs(movement)}`);
+//     }
+// })
+
+// // forEach With Maps and Sets
+// // Map
+// const currencies = new Map([
+//     ['USD', 'United States dollar'],
+//     ['EUR', 'Euro'],
+//     ['GBP', 'Pound sterling'],
+// ]);
+// console.log(currencies)
+// currencies.forEach(function(value, key, map){
+//     console.log(`${key}: ${value}`)
+// })
+
+// // Set
+// const currenciesUnique = new Set(['USD', 'GBP', 'USD', 'EUR', 'EUR'])
+// console.log(currenciesUnique,)
+
+// currenciesUnique.forEach(function(value, _, map){
+//     console.log(`${value}: ${value}`)
+// })
+
+// Julia and Kate are doing a study on dogs. So each of them asked 5 dog owners about their 
+// dog's age, and stored the data into an array (one array for each). For now, they are just 
+// interested in knowing whether a dog is an adult or a puppy. A dog is an adult if it is at
+//  least 3 years old, and it's a puppy if it's less than 3 years old.
+
+// Create a function 'checkDogs', which accepts 2 arrays of dog's ages ('dogsJulia' and 'dogsKate'), and does the following things:
+
+// 1. Julia found out that the owners of the FIRST and the LAST TWO dogs actually have cats,
+//  not dogs! So create a shallow copy of Julia's array, and remove the cat ages from that copied 
+//  array (because it's a bad practice to mutate function parameters)
+
+// 2. Create an array with both Julia's (corrected) and Kate's data
+
+// 3. For each remaining dog, log to the console whether it's an adult ("Dog number 1 is an adult, 
+// and is 5 years old") or a puppy ("Dog number 2 is still a puppy 🐶")
+
+// 4. Run the function for both test datasets
+
+// HINT: Use tools from all lectures in this section so far 😉
+
+// TEST DATA 1: Julia's data [3, 5, 2, 12, 7], Kate's data [4, 1, 15, 8, 3]
+// TEST DATA 2: Julia's data [9, 16, 6, 8, 3], Kate's data [10, 5, 6, 1, 4]
+
+const checkDog = function(arr1, arr2) {
+    let correctedArr = arr1.slice(1, -2)
+    const bothArr = [...correctedArr, ...arr2]
+
+    bothArr.forEach((age, index)=>{
+        let typeDog = age > 3 ? `an adult and is ${age} years old` : 'a puppy 🐶'
+        console.log(`Dog number ${index + 1} is still ${typeDog}`)
+    })
+
 }
 
-console.log('~~~~ FOR EACH ~~~~')
 
-// order for forEach loop --> current element, current index, entire array
-// This is how the arguments are passed into the function
-movements.forEach(function(movement, index, array) {
-    if(movement > 0) {
-        console.log(`Movement ${index +1} You deposited ${movement}`);
-    } else {
-        console.log(`Movement ${index +1} You withdrew ${Math.abs(movement)}`);
-    }
-})
+const Julia = [3, 5, 2, 12, 7]
+const Kate = [4, 1, 15, 8, 3]
 
-// forEach With Maps and Sets
-// Map
-const currencies = new Map([
-    ['USD', 'United States dollar'],
-    ['EUR', 'Euro'],
-    ['GBP', 'Pound sterling'],
-]);
-console.log(currencies)
-currencies.forEach(function(value, key, map){
-    console.log(`${key}: ${value}`)
-})
-
-// Set
-const currenciesUnique = new Set(['USD', 'GBP', 'USD', 'EUR', 'EUR'])
-console.log(currenciesUnique,)
-
-currenciesUnique.forEach(function(value, _, map){
-    console.log(`${value}: ${value}`)
-})
+checkDog(Julia, Kate)
+checkDog([9, 16, 6, 8, 3], [10, 5, 6, 1, 4])
